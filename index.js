0b32e98d1c2e30053b0acd1f0061f462b49ed888
ahmed <saiefeddina@gmail.com>
2021-01-13 12:41:54 +0100

    Connecting to a database

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express =  require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// connect DB
+mongoose.connect('mongodb+srv://ites-jwt:iteslab@my-card.cx3k4.mongodb.net/jwt?retryWrites=true&w=majority', { useUnifiedTopology: true }, ()=>{
+  console.log('connected to db SUCCESS !')  
+})
 
 // Import Routes
 const authRoute = require('./routes/auth');
