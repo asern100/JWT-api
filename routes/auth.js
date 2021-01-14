700ad11b3e5cf07e527fe0d108c8bebb2ba69a20
Ahmed <saiefeddina@gmail.com>
2021-01-14 16:05:28 +0100

    Generate Validation with Joi & Hashing Password

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,33 @@
-const router = require('express').Router();
-const User = require('../model/User');
+const router = require("express").Router();
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+const { registerValidation, loginValidation } = require("../validation");
 
-router.post('/register', async (req,res) => {
+router.post("/register", async (req, res) => {
+  //Validate Data before sending to db server
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
+  //checking if user is already in database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email is already exisits");
+
+  //Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashPasword = await bcrypt.hash(req.body.password, salt);
+
+  //Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashPasword,
   });
   try {
-    
     const savedUser = await user.save();
-    res.send(savedUser);
-
-  } catch (error) {
-    res.status(400).send(err)
+    res.send({ user: user._id });
+  } catch (err) {
+    res.status(400).send(err);
   }
-
-
-
-
 });
 
-module.exports = router ;
+module.exports = router;
